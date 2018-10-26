f97be8ae2d961ee7e925f21d7560881024caf6b1
Cristian Lucero <cristianjlucero32@gmail.com>
2018-10-26 10:17:04 -0700

    This commit will add an if statement for the integer of 1 and turn it to 'boop'. It will also add base of jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,30 @@
 function numberToPhrase(n, customPhrase) {
   var string = n.toString();
   
+  
+  
   if (parseInt(string) === 0) {
     return 'beep';
   }
   
+  if (parseInt(string) === 1) {
+    return 'boop';
+  }
   
 }
 
+console.log(numberToPhrase(10))
 
 
 
 
+$(document).ready(function(){
+  $("button").click(function(event){
+    event.preventDefault();
+    var userInput = $("#userInput").val();
+    $("#output").text(numberToPhrase(userInput));
 
-
+  });
+});
 
 
